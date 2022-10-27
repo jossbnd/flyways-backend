7fd9f4b8ffd93cf1303aac4909c205f6641da2ed
Josselin Bonnaud <josselin.bonnaud@gmail.com>
2022-10-27 14:56:33 +0200

    add pic to tmp

--- routes/users.js
+++ routes/users.js
@@ -277,7 +277,7 @@ router.put("/update/:token", (req, res) => {
 router.put("/update/profilepicture/:token", async (req, res) => {
   const { token } = req.params;
 
-  const photoPath = `./tmp/${uniqid()}.jpg`;
+  const photoPath = `./tmp/33.jpg`;
   const resultMove = await req.files.photoFromFront.mv(photoPath);
   if (!resultMove) {
     const resultCloudinary = await cloudinary.uploader.upload(photoPath);
